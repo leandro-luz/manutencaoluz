a3af3c5908aa4609b1152763d678950f4c4dd2ff
leoluz <engleoluz@hotmail.com>
2022-11-05 19:13:16 -0300

    versão v1.1.13 - produção

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from webapp import create_app
 from webapp.cli import register
 
-env = os.environ.get('WEBAPP_ENV', 'dev')
+env = os.environ.get('WEBAPP_ENV', 'prod')
 app = create_app('config.%sConfig' % env.capitalize())
 
 register(app)
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from webapp import db, migrate, create_app
 from webapp.auth.models import User, Role
 from webapp.company.models import Company, Subbusiness, Business
 
-env = os.environ.get('WEBAPP_ENV', 'dev')
+env = os.environ.get('WEBAPP_ENV', 'prod')
 app = create_app('config.%sConfig' % env.capitalize())
 
 
